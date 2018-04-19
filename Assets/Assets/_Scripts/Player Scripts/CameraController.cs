7f28a5745dfd1dd0fbe82493244388bfb98004b9
Holiday Course <holiday@ad.aie.edu>
2018-04-18 21:00:11 -0700

    added mouse-based player rotation and bullet spawning

---
+++ Assets/Assets/_Scripts/Player Scripts/CameraController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class CameraController : MonoBehaviour {
+
+//    enum States
+//    {
+//        PlayerCam,
+//        ObjectCam,
+//        PostUpInRoom
+//    }
+
+//    public float CameraSpeed = 1;
+
+//    public GameObject Pillar;
+
+//    public PlayerController Player;
+
+//    public Vector3 PlayerOffset;
+
+//    public Vector3 PillarOffset;
+
+//    States CurrentState;
+
+//    void CameraMove(Vector3 Location)
+//    {
+//        transform.position = Vector3.MoveTowards(transform.position, Location, CameraSpeed);
+//    }
+
+//    void CameraShowcase(Vector3 PillarLocation)
+//    {
+//        transform.position = Vector3.MoveTowards(transform.position, PillarLocation, CameraSpeed);
+//    }
+
+//    // Use this for initialization
+//    void Start () {
+//        PlayerOffset = transform.position - Player.transform.position;
+
+//        PillarOffset = transform.position - Pillar.transform.position;
+//    }
+	
+//	// Update is called once per frame
+//	void Update () {
+
+
+
+//        if (Input.GetKey(KeyCode.Q))
+//        {
+//            CurrentState = ObjectCam;
+//        }
+//        else
+//        {
+//            CurrentState = PlayerCam;
+//        }
+
+
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        switch (CurrentState)
+//        {
+//            case States.PlayerCam:
+//                CameraMove(Player.transform.position + PlayerOffset);
+//                break;
+//            case States.ObjectCam:
+//                CameraShowcase(Pillar.transform.position + PillarOffset);
+//                break;
+//            case States.PostUpInRoom:
+//                break;
+//        }
+//    }
+//}
--- Assets/Assets/_Scripts/Player Scripts/PlayerController.cs
+++ Assets/Assets/_Scripts/Player Scripts/PlayerController.cs
@@ -14,10 +14,12 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody RB;
     public Animator Anim; 
     public float MovementSpeed = 100;
-
     public float SprintModifier = 2.5f;
+    public float SmoothDamp = 10;
 
-    public GameObject Cam;
+    public GameObject Bullet;
+
+    public Camera Cam;
 
     public Vector3 IP; // Movement Input
 
@@ -25,6 +27,11 @@ public class PlayerController : MonoBehaviour {
 
     float DT;
 
+
+    public float setAttackDelay = 1.0f;
+    private float attackDelay = 0.0f;
+
+    public bool attacking;
 	// Use this for initialization
 	void Start () {
 
@@ -36,13 +43,14 @@ public class PlayerController : MonoBehaviour {
     {
         IP.x = Input.GetAxisRaw("Horizontal");
         IP.z = Input.GetAxisRaw("Vertical");
+        attacking = Input.GetMouseButton(0);
     }
 
     public void doMovement(float DeltaTime, Vector3 MoveInput, Transform DirTrans)
     {
         if (RB != null)
         {
-            Vector3 Forward = DirTrans.forward * IP.z;
+            Vector3 Forward = DirTrans.forward * IP.z;  //Transform forward multiplied by our vertical input
             Forward.y = 0;
             Forward.Normalize();
 
@@ -68,6 +76,20 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    public void doCombat()
+    {
+        doMovementState();
+
+        attackDelay -= Time.deltaTime;
+
+        if(attacking && attackDelay <= 0.0f)
+        {
+            GameObject temp = Instantiate(Bullet, transform.position, transform.rotation);
+            temp.GetComponent<Rigidbody>().velocity = transform.forward * 20;
+            attackDelay = setAttackDelay;
+        }
+    }
+
     public void updateAnim(Animator AnimationController)
     {
         if (AnimationController != null)
@@ -75,6 +97,23 @@ public class PlayerController : MonoBehaviour {
             var localVel = transform.InverseTransformDirection(RB.velocity);
             AnimationController.SetFloat("ForwardSpeed", localVel.z);
             AnimationController.SetFloat("RightSpeed", localVel.x);
+        }
+    }
+
+    public void doLook() //rotate player towards mouse cursor
+    {
+        RaycastHit hit;
+
+        Ray ray = Cam.ScreenPointToRay(Input.mousePosition); // Creating ray from mouse point on screen
+
+        if(Physics.Raycast(ray, out hit, 1000000)) // casting out a ray and populating our hit vaue
+        {
+
+            Vector3 forward = (transform.position - hit.point).normalized * -1; // getting the direction between our position and or hit.point position
+            forward.y = 0; // zero the y to stay upright
+            forward.Normalize(); // normalize to calculate direction
+            transform.forward = Vector3.MoveTowards(transform.forward, forward, Time.deltaTime * SmoothDamp); //  move our forward towards the direction between the positions
+
         }
     }
 
@@ -102,18 +141,19 @@ public class PlayerController : MonoBehaviour {
 
         updateAnim(Anim);
 
+        doCombat();
 
-        if (CurrentState != States.CombatState)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                CurrentState = States.MovementStateSprint;
-            }
-            else
-            {
-                CurrentState = States.MovementState;
-            }
-        }
+        //if (CurrentState != States.CombatState)
+        //{
+        //    if (Input.GetKey(KeyCode.LeftShift))
+        //    {
+        //        CurrentState = States.MovementStateSprint;
+        //    }
+        //    else
+        //    {
+        //        CurrentState = States.MovementState;
+        //    }
+        //}
 
 
         //doMovement(DT, IP);
@@ -122,6 +162,9 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate()
     {
+
+        doLook();
+
         switch (CurrentState)
         {
             case States.MovementState:
